0f607f96b6c0fe9c77b76a92fd5c70842602a88d
spvvrv <s.pivovarova@rambler.ru>
2023-01-20 16:26:36 +0300

    add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
-  
-}
+const numbers = {
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+    10: "ten",
+    11: "eleven",
+    12: "twelve",
+    13: "thirteen",
+    14: "fourteen",
+    15: "fifteen",
+    16: "sixteen",
+    17: "seventeen",
+    18: "eighteen",
+    19: "nineteen",
+    20: "twenty",
+    30: "thirty",
+    40: "forty",
+    50: "fifty",
+    60: "sixty",
+    70: "seventy",
+    80: "eighty",
+    90: "ninety",
+};
+
+module.exports = function toReadable(num) {
+    if (num === 0) {
+        return "zero";
+    }
+
+    let result = [];
+    const arr = num.toString().split("").reverse();
+
+    let units = +arr[0];
+    let dozens = +arr[1];
+    let hundreds = +arr[2];
+
+    if (hundreds) {
+        result.push(`${numbers[hundreds]} hundred`);
+    }
+    if (dozens > 1) {
+        result.push(numbers[dozens * 10]);
+    }
+    if (dozens === 1) {
+        result.push(numbers[units + 10]);
+    }
+    if (units && dozens !== 1) {
+        result.push(numbers[units]);
+    }
+    return result.join(" ");
+};
